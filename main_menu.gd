9a4d5dc0e930e48061951deddf0e65458098aa37
MI0f <miof333@gmail.com>
2025-03-25 00:15:33 +0000

    fix evrything
    9adit dkxi li khsar

---
+++ main_menu.gd
@@ -0,0 +1,16 @@
+extends Control
+
+func _on_start_game_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_options_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_extras_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
